328466fb33fb44607623b617671e5f5ac45410f4
QyInvoLing <2258191452@qq.com>
2024-04-24 21:31:40 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import './index.less'
+
+const msg = ref('Hello world')
+import mqtt from "mqtt"
+onMounted(()=>{
+  mqtt.connect("mqtt:localhost:25565")
+})
+</script>
